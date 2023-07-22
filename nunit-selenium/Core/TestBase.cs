7b6293052a5bf2764e18550a2b26ee19be5596b5
roberts.kovalauskis <roberts.kovalauskis@visma.com>
2023-07-22 13:49:28 -0700

    Code refactoring

--- nunit-selenium/Core/TestBase.cs
+++ nunit-selenium/Core/TestBase.cs
@@ -1,4 +1,3 @@
-using nunit_selenium.Pages;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -9,73 +8,65 @@ namespace nunit_selenium.Core;
 
 public class TestBase
 {
-     protected IWebDriver Driver;
+    protected IWebDriver Driver;
 
-        [SetUp]
-        public void SetupDriver()
+    [SetUp]
+    public void SetupDriver()
+    {
+        /* Choose WebDriver initialization method */
+        SetupDriverToRunTestsOnSeleniumGrid();
+        // SetupDriverToRunTestsLocally();
+    }
+
+    [TearDown]
+    public void QuitBrowser()
+    {
+        Driver.Quit();
+    }
+
+    /* DON'T delete this method */
+    public void SetupDriverToRunTestsLocally()
+    {
+        Driver = new ChromeDriver();
+        Driver.Manage().Window.Maximize();
+    }
+
+    /* DON'T delete this method */
+    public void SetupDriverToRunTestsOnSeleniumGrid()
+    {
+        string host = "localhost";
+        DriverOptions options;
+
+        if (Environment.GetEnvironmentVariable("BROWSER") != null &&
+            Environment.GetEnvironmentVariable("BROWSER")
+                .Equals("firefox", StringComparison.OrdinalIgnoreCase))
         {
-            /* Choose WebDriver initialization method */
-            SetupDriverToRunTestsOnSeleniumGrid();
-            // SetupDriverToRunTestsLocally();
-
-            InitializePageObjects(); // (only after the WebDriver is initialised)
+            options = new FirefoxOptions();
+        }
+        else
+        {
+            options = new ChromeOptions(); // use Chrome by default
         }
 
-        [TearDown]
-        public void QuitBrowser()
+        if (Environment.GetEnvironmentVariable("HUB_HOST") != null)
         {
-            Driver.Quit();
+            host = Environment.GetEnvironmentVariable("HUB_HOST");
         }
 
-        /* DON'T delete this method */
-        public void SetupDriverToRunTestsLocally()
+        string completeUrl = "http://" + host + ":4444/wd/hub";
+
+        // RemoteWebDriver doesn't know what browser we need to use 
+        // that's why we specify options (DriverOptions) 
+        Driver = new RemoteWebDriver(new Uri(completeUrl), options);
+
+        /* The Local File Detector allows the transfer of files
+        from the client machine to the remote server. */
+        var allowsDetection = Driver as IAllowsFileDetection;
+        if (allowsDetection != null)
         {
-            Driver = new ChromeDriver();
+            allowsDetection.FileDetector = new LocalFileDetector();
         }
 
-        /* DON'T delete this method */
-        public void SetupDriverToRunTestsOnSeleniumGrid() 
-        {
-            string host = "localhost";
-            DriverOptions options;
-
-            if (Environment.GetEnvironmentVariable("BROWSER") != null &&
-                Environment.GetEnvironmentVariable("BROWSER")
-                    .Equals("firefox", StringComparison.OrdinalIgnoreCase))
-            {
-                options = new FirefoxOptions();
-            }
-            else
-            {
-                options = new ChromeOptions(); // use Chrome by default
-            }
-
-            if (Environment.GetEnvironmentVariable("HUB_HOST") != null)
-            {
-                host = Environment.GetEnvironmentVariable("HUB_HOST");
-            }
-
-            string completeUrl = "http://" + host + ":4444/wd/hub";
-
-            // RemoteWebDriver doesn't know what browser we need to use 
-            // that's why we specify options (DriverOptions) 
-            Driver = new RemoteWebDriver(new Uri(completeUrl), options);
-
-            /* The Local File Detector allows the transfer of files
-            from the client machine to the remote server. */
-            var allowsDetection = Driver as IAllowsFileDetection;
-            if (allowsDetection != null)
-            {
-                allowsDetection.FileDetector = new LocalFileDetector();
-            }
-
-            Driver.Manage().Window.Maximize();
-        }
-        
-        protected LoginPage _LoginPage;
-
-        protected void InitializePageObjects()
-        {
-            _LoginPage = new LoginPage(Driver);
-        }
+        Driver.Manage().Window.Maximize();
+    }
 }
